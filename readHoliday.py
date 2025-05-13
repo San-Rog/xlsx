8742677c29e259b5fedc595ebd2e0ed49be00a01
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-13 00:38:13 -0300

    Update readHoliday.py

--- readHoliday.py
+++ readHoliday.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import streamlit as st
+from datetime import date
 
-df = pd.read_csv('feriadosNacionais.csv')  
+def changeValue(value):
+    try:
+        return (value.strftime("%d/%m/%Y"))
+    except:
+        return ""
+
+df = pd.read_csv('feriadosNacionais.csv')
+df["Data"] = df["Data"].apply(changeValue)
 st.title("Hello world!") 
 st.write(df) 
